19e837bc79eb64b48e98c3f709e579d0249fa88e
wendylee-rawr <wendy.lee@unsw.edu.au>
2025-09-29 13:41:00 +1000

    DHT22_sensor_only
    Code to read air temperature and humidity using a DHT22 sensor, then print it on the Serial Monitor

---
+++ DHT22_code.ino
@@ -0,0 +1,33 @@
+/*  Temperature humidity sensor */
+#include <DHT.h>
+#define datapin 7     // Digital  pin we're connected to
+#define DHTTYPE DHT22
+DHT dht(datapin, DHTTYPE);
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+  dht.begin(); 
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  /* Find Temperature & Humidity */
+  float air_temp = dht.readTemperature();
+  float humidity = dht.readHumidity();
+
+ //Print temp and humidity values to serial monitor
+  Serial.print("Humidity: ");
+  Serial.print(humidity);
+  Serial.print(" %, Temp: ");
+  Serial.print(air_temp);
+  Serial.println(" Celsius");
+  
+  delay(2000);
+/* // Print for graphing
+  Serial.print(humidity);
+  Serial.print(",");
+  Serial.print(air_temp);
+  Serial.println();
+*/
+}
